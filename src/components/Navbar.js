1da87acc8e06efe2c1665b6c049cb851f22e6a75
Jesse Tomchak <jtomchak@gmail.com>
2020-09-04 15:35:09 -0700

    fix: route name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -47,7 +47,7 @@ export default function Navbar() {
         <NavLink ml={4} href="/">
           Home
         </NavLink>
-        <NavLink ml={4} href="/blog">
+        <NavLink ml={4} href="/post">
           Media Jams
         </NavLink>
       </Box>
