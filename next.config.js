337a9bfbdfb6754ebfe95d840cc8b9d488ce9c12
Jesse Tomchak <jtomchak@gmail.com>
2020-10-13 11:01:00 -0700

    feature: working sso nextjs-auth0

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ module.exports = {
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
     CLOUD_NAME: process.env.CLOUD_NAME,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
   },
   publicRuntimeConfig: {
     // Will be available on both server and client
