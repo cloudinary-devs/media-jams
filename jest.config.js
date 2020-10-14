7cf3dcb35ae67e2f58178835db95538670adb4c3
Jesse Tomchak <jtomchak@gmail.com>
2020-10-14 10:28:08 -0700

    fix: jest file grew, separate file now

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  setupFilesAfterEnv: [
+    '<rootDir>/setupTests.js',
+    '<rootDir>/__mocks__/matchMediaMock.js',
+  ],
+  setupFiles: ['dotenv/config'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '^@theme$': '<rootDir>/src/theme$1',
+    '^@components(.*)$': '<rootDir>/src/components$1',
+    '^@hooks(.*)$': '<rootDir>/src/hooks$1',
+  },
+};
