b8f64b47fb99b523f125a45efeb14586e79eae16
Jesse Tomchak <jtomchak@gmail.com>
2020-10-27 11:58:26 -0700

    fix: set to 'NEXT_PUBLIC' publicRuntime seems throws on test

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   env: {
+    SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
     CLOUD_NAME: process.env.CLOUD_NAME,
@@ -11,7 +13,5 @@ module.exports = {
     // Will be available on both server and client
     staticFolder: '/static',
     mixPanelToken: process.env.MIXPANEL_TOKEN,
-    SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    SANITY_DATASET: process.env.SANITY_DATASET,
   },
 };
