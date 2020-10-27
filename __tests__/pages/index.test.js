cf71c1d7e92adcb2f4200cc9832e4485250ecc37
Jesse Tomchak <jtomchak@gmail.com>
2020-10-27 15:52:40 -0700

    fix: jest mock sanity client

--- __tests__/pages/index.test.js
+++ __tests__/pages/index.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { render, screen, act, waitFor } from '@testing-library/react';
 import Index from '../../pages/index.js';
 
+// we don't need sanity api to initialize.
+jest.mock('@sanity/client');
+
 test('renders index w/o error', async () => {
   render(<Index />);
   const heroElement = await waitFor(() =>
