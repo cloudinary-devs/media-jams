715298d144f1cfe38bc297a27577a7736b9e7a0e
Jesse Tomchak <jtomchak@gmail.com>
2020-11-09 13:11:53 -0700

    fix: fully qualified URL

--- studio/scripts/accessControl.js
+++ studio/scripts/accessControl.js
@@ -10,64 +10,85 @@ const r2 = require('r2');
  * TODO: create a sync script to fetch all members from production and update members on stage
  */
 
-const mutations = [
-  {
-    createOrReplace: {
-      _id: '_.groups.creator',
-      _type: 'system.group',
-      grants: [
-        {
-          filter: "_type == 'post'",
-          permissions: ['read', 'update', 'create'],
-        },
-        {
-          filter: "_type == 'workflow.metadata'",
-          permissions: ['read', 'update', 'create'],
-        },
-        {
-          filter: "_type == 'tag'",
-          permissions: ['read'],
-        },
-        {
-          filter: "_type == 'category'",
-          permissions: ['read'],
-        },
-        {
-          filter: "_type == 'author'",
-          permissions: ['read', 'update', 'create'],
-        },
-        {
-          filter: '_id in path("**")',
-          permissions: ['read', 'create', 'update'],
-        },
-      ],
-      members: [
-        'e-a5ce59720555a32a37fc4b93a30bfed0',
-        'e-11f130d2d23ffb63950957c0831e8762',
-        'e-5369fe0e1fd16b4055e3772820d56f1f',
-        'e-39f2223a237d3ce5eeb9c2d316a22351',
-      ], //jtomchak@gmail.com
-    },
-  },
-  {
-    createOrReplace: {
-      _id: '_.groups.moderator',
-      _type: 'system.group',
-      grants: [
-        {
-          filter: '_id in path("**")',
-          permissions: ['create', 'update', 'read', 'editHistory', 'history'],
-        },
-      ],
-      members: [
-        'e-093e43c0f098447a554b0ba08f0a4c92',
-        'e-5369fe0e1fd16b4055e3772820d56f1f',
-        'e-1ce8d42c0f8988a83fe2529c25bfdf3f',
-        'e-28e5c40c39fd1d448cb08a2afc57454c',
-      ],
-    },
-  },
-];
+// const mutations = [
+//   {
+//     createOrReplace: {
+//       _id: '_.groups.creator',
+//       _type: 'system.group',
+//       grants: [
+//         {
+//           filter: "_type == 'post'",
+//           permissions: ['read', 'update', 'create'],
+//         },
+//         {
+//           filter: "_type == 'workflow.metadata'",
+//           permissions: ['read', 'update', 'create'],
+//         },
+//         {
+//           filter: "_type == 'tag'",
+//           permissions: ['read'],
+//         },
+//         {
+//           filter: "_type == 'category'",
+//           permissions: ['read'],
+//         },
+//         {
+//           filter: "_type == 'author'",
+//           permissions: ['read', 'update', 'create'],
+//         },
+//         {
+//           filter: '_id in path("**")',
+//           permissions: ['read', 'create', 'update'],
+//         },
+//         {
+//           path: 'drafts.**',
+//           permissions: ['read'],
+//         },
+//       ],
+//       members: [
+//         'e-a5ce59720555a32a37fc4b93a30bfed0',
+//         'e-11f130d2d23ffb63950957c0831e8762',
+//         'e-5369fe0e1fd16b4055e3772820d56f1f',
+//         'e-39f2223a237d3ce5eeb9c2d316a22351',
+//       ], //jtomchak@gmail.com
+//     },
+//   },
+//   {
+//     createOrReplace: {
+//       _id: '_.groups.moderator',
+//       _type: 'system.group',
+//       grants: [
+//         {
+//           filter: '_id in path("**")',
+//           permissions: ['create', 'update', 'read', 'editHistory', 'history'],
+//         },
+//       ],
+//       members: [
+//         'e-093e43c0f098447a554b0ba08f0a4c92',
+//         'e-5369fe0e1fd16b4055e3772820d56f1f',
+//         'e-1ce8d42c0f8988a83fe2529c25bfdf3f',
+//         'e-28e5c40c39fd1d448cb08a2afc57454c',
+//       ],
+//     },
+//   },
+// ];
+
+// const mutations = [
+//   {
+//     patch: {
+//       id: '_.groups.moderator',
+//       insert: {
+//         after: 'grants[-1]',
+//         items: [
+//           {
+//             path: 'drafts.**',
+//             permissions: ['create', 'update', 'read'],
+//           },
+//         ],
+//       },
+//     },
+//   },
+// ];
 
 const updateAccess = async () => {
   try {
