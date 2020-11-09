3e24b8f788c1f19dd339ea6ba056def375bbd056
Jesse Tomchak <jtomchak@gmail.com>
2020-11-09 12:52:49 -0700

    fix: process.env.VERCEL_URL as baseURL

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -10,7 +10,6 @@ import auth0 from '@lib/auth0';
 export default async function login(req, res) {
   try {
     const user = await auth0.handleLogin(req, res);
-    console.log(user);
   } catch (error) {
     console.error(error);
     res.status(error.status || 400).end(error.message);
--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -2,7 +2,7 @@ import auth0 from '../../../lib/auth0';
 
 export default async function logout(req, res) {
   try {
-    await auth0.handleLogout(req, res, { returnTo: 'http://localhost:3000/' });
+    await auth0.handleLogout(req, res, { returnTo: process.env.VERCEL_URL });
   } catch (error) {
     console.error(error);
     res.status(error.status || 400).end(error.message);
