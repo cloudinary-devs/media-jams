fcf9de57c29241760370c157544aaec327a43215
Jesse Tomchak <jtomchak@gmail.com>
2020-11-16 16:02:28 -0700

    fix: profile page
    update auto generate new studio link in profile

---
+++ pages/api/auth/studio.js
@@ -0,0 +1,53 @@
+import fetch from 'isomorphic-unfetch';
+
+const studioURL = 'https://5ad74sb4.api.sanity.io/v1/auth/thirdParty/session';
+const role = {
+  MODERATOR: 'moderator',
+  CREATOR: 'creator',
+};
+const userData = (user, role) => ({
+  userId: `e-${user.sub.split('|').pop()}`,
+  userFullName: user.nickname,
+  userEmail: user.email,
+  userRole: role === role.MODERATOR ? 'administrator' : 'editor',
+  userImage: user.picture,
+});
+const sessionQuery = (data) => ({
+  method: 'POST',
+  headers: {
+    Authorization: `Bearer ${process.env.SANITY_SESSION_ROBOT}`,
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+});
+
+const studioAuth = async (req, res) => {
+  try {
+    // const recipes = await getRecipes();
+    res.status(200).json({ success: true });
+  } catch (error) {
+    res.status(500).json({ success: false });
+  }
+};
+
+export const generateSanitySession = async (user) => {
+  const assignedRoles = user['https://mediajams-studio/roles'];
+  const userRole = assignedRoles.includes(role.MODERATOR)
+    ? role.MODERATOR
+    : assignedRoles.includes(role.CREATOR)
+    ? role.CREATOR
+    : null;
+  if (!userRole) throw new Error('No roles for user.');
+  try {
+    const response = await fetch(
+      studioURL,
+      sessionQuery(userData(user, userRole)),
+    );
+    const session = response.ok ? await response.json() : null;
+    return session;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export default studioAuth;
--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,11 @@
 // This import is only needed when checking authentication status directly from getServerSideProps
-// import auth0 from '@lib/auth0';
+import auth0 from '@lib/auth0';
 import { Flex, Text, Box, Link, Icon } from '@chakra-ui/core';
 import { Link as NextLink } from 'next/link';
 import Layout from '@components/Layout';
 import { useFetchUser } from '@lib/user';
+
+import { generateSanitySession } from './api/auth/studio';
 
 function ProfileCard({ user }) {
   return (
@@ -16,27 +18,52 @@ function ProfileCard({ user }) {
         <p>nickname: {user.nickname}</p>
         <p>name: {user.name}</p>
       </div>
-      <Link as={NextLink} px={2} href="/api/auth/logout">
-        Logout
-      </Link>
+
       <Link
-        href={`${user['https://mediajams-studio/claimUrl']}?origin=https://mediajams.sanity.studio/`}
+        href={`${user['https://mediajams-studio/claimUrl']}?origin=https://mediajams.sanity.studio`}
         isExternal
       >
         Media Jams Studio <Icon name="external-link" mx="2px" />
+      </Link>
+      <Link as={NextLink} px={2} href="/api/auth/logout">
+        Logout
       </Link>
     </>
   );
 }
 
-function Profile() {
-  const { user, loading } = useFetchUser({ required: true });
+function Profile({ user }) {
+  // const { user, loading } = useFetchUser({ required: true });
 
   return (
-    <Layout user={user} loading={loading}>
-      {loading ? <>Loading...</> : user && <ProfileCard user={user} />}
+    <Layout>
+      <ProfileCard user={user} />
     </Layout>
   );
+}
+
+/**
+ * Get User Session SSR or redirect to Login
+ * Generate Sanity Session for Link to Studio
+ *
+ */
+export async function getServerSideProps({ req, res }) {
+  // Here you can check authentication status directly before rendering the page,
+  // however the page would be a serverless function, which is more expensive and
+  // slower than a static page with client side authentication
+  const session = await auth0.getSession(req);
+
+  if (!session || !session.user) {
+    res.writeHead(302, {
+      Location: '/api/auth/login',
+    });
+    res.end();
+    return;
+  }
+  const { user } = session;
+  const sanitySession = (await generateSanitySession(user)) ?? null;
+
+  return { props: { user, sanitySession } };
 }
 
 export default Profile;
