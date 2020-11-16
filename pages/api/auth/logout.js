c8d14830393067d9800a35348df5c952d633cf42
Jesse Tomchak <jtomchak@gmail.com>
2020-11-16 15:03:53 -0700

    fix: logout redirect

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -1,11 +1,13 @@
 import auth0 from '../../../lib/auth0';
+import { logoutStudio } from '@lib/api';
 
 export default async function logout(req, res) {
   try {
+    logoutStudio();
     await auth0.handleLogout(req, res, {
       returnTo:
         process.env.NODE_ENV === 'production'
-          ? `https://${process.env.VERCEL_URL}/`
+          ? `https://mediajams.dev`
           : `http://localhost:3000`,
     });
   } catch (error) {
