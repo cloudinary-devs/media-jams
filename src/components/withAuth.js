200186c788a5b534b646353bcdd7b9a0430de9de
Jesse Tomchak <jtomchak@gmail.com>
2020-12-02 14:45:28 -0700

    fix: ❓is the error thrown in getSession?

--- src/components/withAuth.js
+++ src/components/withAuth.js
@@ -5,15 +5,16 @@ import createLoginUrl from '@lib/login-url';
 
 export function withAuthServerSideProps(getServerSidePropsFunc) {
   return async (context) => {
-    const session = await auth0.getSession(context.req);
-    if (!session || !session.user) {
-      context.res.writeHead(302, {
-        Location: createLoginUrl(context.req.url),
-      });
-      context.res.end();
-      return;
-    }
-    const { user } = session;
+    // const session = await auth0.getSession(context.req);
+    // if (!session || !session.user) {
+    //   context.res.writeHead(302, {
+    //     Location: createLoginUrl(context.req.url),
+    //   });
+    //   context.res.end();
+    //   return;
+    // }
+    // const { user } = session;
+    const user = {};
     if (getServerSidePropsFunc) {
       return {
         props: { user, data: await getServerSidePropsFunc(context, user) },
