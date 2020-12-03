ff0a1a7a7dbeb383575695acee14126d99f6e1bd
Jesse Tomchak <jtomchak@gmail.com>
2020-12-03 10:11:56 -0700

    fix: api auth redirect 🐎

--- src/components/withAuth.js
+++ src/components/withAuth.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 import auth0 from '@lib/auth0';
 import createLoginUrl from '@lib/login-url';
 
+/**
+ *
+ * @param {Function} getServerSidePropsFunc
+ */
 export function withAuthServerSideProps(getServerSidePropsFunc) {
   return async (context) => {
     const session = await auth0.getSession(context.req);
     if (!session) {
       context.res.writeHead(302, {
-        Location: createLoginUrl(context.req.url),
+        Location: '/api/auth/login',
       });
       context.res.end();
       return { props: {} };
