2a0eb4ccca7d6cb58d5407c5edda2932f669b305
Jesse Tomchak <jtomchak@gmail.com>
2020-12-03 09:54:08 -0700

    fix: return empty props. 🐇

--- src/components/withAuth.js
+++ src/components/withAuth.js
@@ -6,18 +6,22 @@ import createLoginUrl from '@lib/login-url';
 export function withAuthServerSideProps(getServerSidePropsFunc) {
   return async (context) => {
     const session = await auth0.getSession(context.req);
-    // if (!session || !session.user) {
-    //   context.res.writeHead(302, {
-    //     Location: createLoginUrl(context.req.url),
-    //   });
-    //   context.res.end();
-    //   return;
-    // }
+    if (!session) {
+      context.res.writeHead(302, {
+        Location: createLoginUrl(context.req.url),
+      });
+      context.res.end();
+      return { props: {} };
+    }
     // const { user } = session;
+    console.log(session);
     const user = {};
     if (getServerSidePropsFunc) {
       return {
-        props: { user, data: await getServerSidePropsFunc(context, user) },
+        props: {
+          user,
+          data: await getServerSidePropsFunc(context, session.user),
+        },
       };
     }
     return { props: { user, data: { props: { user } } } };
