25ce738021db749e031d7889a3d2d4ab2bca5963
Jesse Tomchak <jtomchak@gmail.com>
2021-01-12 15:33:26 -0700

    fix: image background hero mobile

--- src/components/JamContentHero.js
+++ src/components/JamContentHero.js
@@ -27,10 +27,10 @@ export default function JamContentHero({
   author,
   children,
 }) {
-  const headings = useBreakpointValue({
-    base: 'md',
-    md: 'xl',
-    lg: '2xl',
+  const isMobile = useBreakpointValue({
+    base: true,
+    md: true,
+    lg: false,
   });
 
   const { generateImageUrl } = useImage('mediadevs');
@@ -44,35 +44,58 @@ export default function JamContentHero({
   };
 
   return (
-    <Flex h="lg" direction="row" justifyContent="center" alignItems="center">
-      <Box flex={{ sm: 1, base: 0 }} boxSize="sm" overflow="hidden">
-        <Image
-          maxWidth="100%"
-          alt="Feature Image"
-          src={generateImageUrl(imgConfig)}
-        />
-      </Box>
-      <HStack height="100%" width="100%" flex={2}>
-        <VStack align="stretch" flex={1}>
-          <Box backgroundColor="yellow.400" height="100%" py={4}>
-            <Box
-              backgroundColor="blue.200"
-              ml="-10%"
-              width="110%"
-              display="block"
-              py={4}
-            >
-              <Box color="grey.900" my={4} pl={4}>
-                <Heading as="h1" fontSize="4xl" textStyle="headline-accent">
-                  {title}
-                </Heading>
-                <AuthorByline fontSize="xs">By {author?.name}</AuthorByline>
-                <Text maxWidth="80%">{description}</Text>
-              </Box>
+    <>
+      {isMobile ? (
+        <Flex direction="column" justifyContent="center" alignItems="center">
+          <Box
+            backgroundImage={`url(${generateImageUrl(imgConfig)})`}
+            backgroundSize="cover"
+          >
+            <Box backgroundColor="blue.200" opacity="90%">
+              <Heading as="h1" fontSize="4xl" textStyle="headline-accent">
+                {title}
+              </Heading>
+              <Text maxWidth="80%">{description}</Text>
             </Box>
           </Box>
-        </VStack>
-      </HStack>
-    </Flex>
+        </Flex>
+      ) : (
+        <Flex
+          h="lg"
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Box flex={{ sm: 1, base: 0 }} overflow="hidden">
+            <Image
+              maxWidth="100%"
+              alt="Feature Image"
+              src={generateImageUrl(imgConfig)}
+            />
+          </Box>
+          <HStack height="100%" width="100%" flex={2}>
+            <VStack align="stretch" flex={1}>
+              <Box backgroundColor="yellow.400" height="100%" py={4}>
+                <Box
+                  backgroundColor="blue.200"
+                  ml="-10%"
+                  width="110%"
+                  display="block"
+                  py={4}
+                >
+                  <Box color="grey.900" my={4} pl={4}>
+                    <Heading as="h1" fontSize="4xl" textStyle="headline-accent">
+                      {title}
+                    </Heading>
+                    <AuthorByline fontSize="xs">By {author?.name}</AuthorByline>
+                    <Text maxWidth="80%">{description}</Text>
+                  </Box>
+                </Box>
+              </Box>
+            </VStack>
+          </HStack>
+        </Flex>
+      )}
+    </>
   );
 }
