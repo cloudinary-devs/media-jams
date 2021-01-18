5cd70a325274c833e2171b0c88ce621d79afb156
Jesse Tomchak <jtomchak@gmail.com>
2021-01-18 12:10:02 -0700

    fix: flush

--- pages/api/error-test.js
+++ pages/api/error-test.js
@@ -1,11 +1,18 @@
 import { initSentry } from '@lib/sentry';
-//initialize Sentry
+import * as Sentry from '@sentry/node';
+
 initSentry();
 
-const doAsyncWork = () => Promise.reject(new Error('API Error Testing 1'));
-doAsyncWork();
-
 export default async function handler(req, res) {
+  try {
+    throw new Error('API Test ');
+  } catch (error) {
+    Sentry.captureException(error);
+  }
+
+  // Flushing before returning is necessary if deploying to Vercel, see
+  // https://vercel.com/docs/platform/limits#streaming-responses
+  await Sentry.flush(2000);
   res.statusCode = 200;
   res.json({ name: 'John Doe' });
 }
