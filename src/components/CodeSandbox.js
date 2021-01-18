d4129f3b35127a0c52a4ea9d625d15ae3073cba6
Jesse Tomchak <jtomchak@gmail.com>
2021-01-18 10:30:59 -0700

    fix: codeSandbox

--- src/components/CodeSandbox.js
+++ src/components/CodeSandbox.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { useColorMode, IconButton, useColorModeValue } from '@chakra-ui/react';
+import {
+  useColorMode,
+  IconButton,
+  useColorModeValue,
+  Flex,
+  Box,
+} from '@chakra-ui/react';
 import Iframe from '@components/Iframe';
 
 const isCodeSandboxUrl = (url) => new URL(url).hostname === 'codesandbox.io';
@@ -15,17 +21,21 @@ export default function CodeSandbox({ title, src, children, ...props }) {
     <>
       {/* if it's not an iframe from codesandbox, return unchanged */}
       {!isCodeSandboxUrl(src) && <Iframe src={src} title={title} {...props} />}
-      <Iframe
-        src={codeSandboxEncodeUrl(src)}
-        title={title}
-        width="full"
-        maxW="1280px"
-        mx="auto"
-        minH="800px"
-        overflow="hidden"
-        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-      ></Iframe>
+      <Flex justifyContent="center">
+        <Box
+          as="iframe"
+          allowFullScreen
+          src={codeSandboxEncodeUrl(src)}
+          title={title}
+          width="full"
+          maxW="1280px"
+          mx="auto"
+          minH="800px"
+          overflow="hidden"
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        ></Box>
+      </Flex>
     </>
   );
 }
