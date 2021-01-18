7aa79d231419e5b2d7ce8a76892d7ff918ec63b8
Jesse Tomchak <jtomchak@gmail.com>
2021-01-18 13:00:02 -0700

    fix: test match text

--- __tests__/components/hero.test.js
+++ __tests__/components/hero.test.js
@@ -4,8 +4,6 @@ import Hero from '@components/Hero';
 
 test('renders hero component', () => {
   render(<Hero />);
-  const heroElementFirst = screen.getAllByText(/Putting Media to Work/);
-  const heroElementSecond = screen.getAllByText(/IS HARD/);
-  expect(heroElementFirst[0]).toBeInTheDocument();
-  expect(heroElementSecond[0]).toBeInTheDocument();
+  const heroElementFirst = screen.getByText(/Putting Media to Work Is Hard/);
+  expect(heroElementFirst).toBeInTheDocument();
 });
