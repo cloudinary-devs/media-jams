d92b9407ec55ad566c9d44dfd6914bddd51be386
Jesse Tomchak <jtomchak@gmail.com>
2021-01-27 15:45:24 -0700

    fix: need audience and response type to get JWT for AIP calls

--- pages/profile.js
+++ pages/profile.js
@@ -61,7 +61,7 @@ export async function getServerSideProps({ req, res }) {
   // however the page would be a serverless function, which is more expensive and
   // slower than a static page with client side authentication
   const session = await auth0.getSession(req);
-
+  console.log(session);
   if (!session || !session.user) {
     res.writeHead(302, {
       Location: '/api/auth/login',
