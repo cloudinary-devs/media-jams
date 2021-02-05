2684cb32bbf88dd397088a77aadaf6a4df274700
Jesse Tomchak <jtomchak@gmail.com>
2021-02-05 16:46:44 -0700

    fix: utils path added for Jest

--- jest.config.js
+++ jest.config.js
@@ -23,6 +23,7 @@ module.exports = {
     '^@components(.*)$': '<rootDir>/src/components$1',
     '^@hooks(.*)$': '<rootDir>/src/hooks$1',
     '^@lib(.*)$': '<rootDir>/lib$1',
+    '^@utils(.*)$': '<rootDir>/src/utils$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
