ebee9d2701397de22795ddfd2121ea12e0cc7ca0
Jesse Tomchak <jtomchak@gmail.com>
2021-02-11 15:28:57 -0700

    fix: Next/Image setup

--- jest.setup.js
+++ jest.setup.js
@@ -27,3 +27,16 @@ window.matchMedia = jest.fn().mockImplementation((query) => ({
   removeEventListener: jest.fn(),
   dispatchEvent: jest.fn(),
 }));
+
+// Specificlly to address Next/Image
+// https://github.com/vercel/next.js/discussions/18373
+process.env = {
+  ...process.env,
+  __NEXT_IMAGE_OPTS: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [],
+    domains: ['res.cloudinary.com'],
+    path: '/_next/image',
+    loader: 'default',
+  },
+};
