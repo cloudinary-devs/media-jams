caa4cb49eb9834917cdbed5cb045c945deee06b0
Jesse Tomchak <jtomchak@gmail.com>
2021-03-09 14:26:23 -0700

    feat: Codesandbox mdx component

--- src/components/CodeSandbox.js
+++ src/components/CodeSandbox.js
@@ -6,36 +6,32 @@ import {
   Flex,
   Box,
 } from '@chakra-ui/react';
-import Iframe from '@components/Iframe';
 
-const isCodeSandboxUrl = (url) => new URL(url).hostname === 'codesandbox.io';
-
-export default function CodeSandbox({ title, src, children, ...props }) {
+export default function CodeSandbox({
+  title,
+  name,
+  view = '',
+  children,
+  ...props
+}) {
   const { colorMode = 'dark' } = useColorMode();
-  const codeSandboxEncodeUrl = (src) => {
-    const url = new URL(src);
-    url.search = `runonclick=1&codemiror=1&fontsize=14&hidenavigation=1&theme=${!colorMode}`;
-    return url;
-  };
+  const urlWithOptions = `https://codesandbox.io/embed/${name}?runonclick=1&autoresize=1&codemirror=1&fontsize=14&hidenavigation=1&theme=${!colorMode}&view=${view}`;
+
   return (
-    <>
-      {/* if it's not an iframe from codesandbox, return unchanged */}
-      {!isCodeSandboxUrl(src) && <Iframe src={src} title={title} {...props} />}
-      <Flex justifyContent="center">
-        <Box
-          as="iframe"
-          allowFullScreen
-          src={codeSandboxEncodeUrl(src)}
-          title={title}
-          width="full"
-          maxW="1280px"
-          mx="auto"
-          minH="800px"
-          overflow="hidden"
-          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-        ></Box>
-      </Flex>
-    </>
+    <Flex justifyContent="center">
+      <Box
+        as="iframe"
+        allowFullScreen
+        src={urlWithOptions}
+        title={title}
+        width="full"
+        maxW="1280px"
+        mx="auto"
+        minH="800px"
+        overflow="hidden"
+        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+      ></Box>
+    </Flex>
   );
 }
