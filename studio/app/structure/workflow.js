3b6c075fc6232bcc01eb8e1f326b3321782b8113
Jesse Tomchak <jtomchak@gmail.com>
2021-03-10 14:26:41 -0700

    feat: filtered folders for workflow

--- studio/app/structure/workflow.js
+++ studio/app/structure/workflow.js
@@ -3,6 +3,7 @@ import { filter, map, switchMap } from 'rxjs/operators';
 import { getDocumentMutations$ } from '../lib/document';
 import { getDocumentQuery$ } from '../lib/document';
 import { getCurrentUser$ } from '../lib/user';
+import { MdThumbUp, MdSync, MdRateReview } from 'react-icons/md';
 
 const WORKFLOW_DOCUMENTS_FILTER = `_type == $type && $state == state`;
 const WORKFLOW_DOCUMENTS_QUERY = `
@@ -14,36 +15,63 @@ const WORKFLOW_DOCUMENTS_QUERY = `
   }  
 `;
 
+const workFlowDirectories = [
+  {
+    title: 'In Review',
+    id: 'in-review',
+    subTitle: 'Needs Review',
+    icon: MdRateReview,
+    state: 'inReview',
+  },
+  {
+    title: 'Changes Requested',
+    id: 'in-change',
+    subTitle: 'Pending Author Changes',
+    icon: MdSync,
+    state: 'changesRequested',
+  },
+  {
+    title: 'Approved',
+    id: 'approved',
+    subTitle: 'Approved, ready to publish',
+    icon: MdThumbUp,
+    state: 'approved',
+  },
+];
+
 export const workflowListItems = [
-  S.listItem()
-    .title('In Review')
-    .id('in-review')
-    .child(() => {
-      return getCurrentUser$().pipe(
-        filter(Boolean),
-        switchMap((user) => {
-          return getDocumentMutations$(WORKFLOW_DOCUMENTS_FILTER, {
-            type: 'workflow.metadata',
-            state: 'inReview',
-          }).pipe(
-            switchMap(() => {
-              return getDocumentQuery$(WORKFLOW_DOCUMENTS_QUERY, {
-                type: 'workflow.metadata',
-                state: 'inReview',
-              });
-            }),
-          );
-        }),
-        map((docs) => {
-          return S.list()
-            .title(docs.length ? 'Needs Review' : 'All Clear')
-            .id('in-review')
-            .items(
-              docs.map((item) =>
-                S.documentListItem().id(item._id).schemaType(item._type),
-              ),
+  ...workFlowDirectories.map(({ title, id, subTitle, state, icon }) =>
+    S.listItem()
+      .title(title)
+      .id(id)
+      .icon(icon)
+      .child(() => {
+        return getCurrentUser$().pipe(
+          filter(Boolean),
+          switchMap((user) => {
+            return getDocumentMutations$(WORKFLOW_DOCUMENTS_FILTER, {
+              type: 'workflow.metadata',
+              state: state,
+            }).pipe(
+              switchMap(() => {
+                return getDocumentQuery$(WORKFLOW_DOCUMENTS_QUERY, {
+                  type: 'workflow.metadata',
+                  state: state,
+                });
+              }),
             );
-        }),
-      );
-    }),
+          }),
+          map((docs) => {
+            return S.list()
+              .title(docs.length ? subTitle : 'All Clear')
+              .id(id)
+              .items(
+                docs.map((item) =>
+                  S.documentListItem().id(item._id).schemaType(item._type),
+                ),
+              );
+          }),
+        );
+      }),
+  ),
 ];
