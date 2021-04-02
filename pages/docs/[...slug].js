c903487e45dc7cf039f2b7ddff90454873de7772
DomitriusAnthony <domitriusaclark@gmail.com>
2021-04-01 20:53:46 -0400

    Fixing possible fs import problems

--- pages/docs/[...slug].js
+++ pages/docs/[...slug].js
@@ -1,9 +1,4 @@
-import path from 'path';
-import renderToString from 'next-mdx-remote/render-to-string';
 import hydrate from 'next-mdx-remote/hydrate';
-import matter from 'gray-matter';
-import glob from 'fast-glob';
-
 import { Box, chakra } from '@chakra-ui/react';
 import PageTransition from '@components/PageTransition';
 
@@ -27,13 +22,17 @@ export default function Data({ mdxSource, frontMatter }) {
 // This glob is what will be used to generate static routes
 const contentPath = 'src/documentation';
 export const contentGlob = `${contentPath}/**/*.mdx`;
-export const getFileSlug = (filePath) => {
-  const filename = filePath.replace(`${contentPath}/`, '');
-  const slug = filename.replace(new RegExp(path.extname(filePath) + '$'), '');
-  return slug;
-};
 
 export async function getStaticPaths() {
+  const path = require('path');
+  const glob = require('fast-glob');
+
+  export const getFileSlug = (filePath) => {
+    const filename = filePath.replace(`${contentPath}/`, '');
+    const slug = filename.replace(new RegExp(path.extname(filePath) + '$'), '');
+    return slug;
+  };
+
   const files = glob.sync(contentGlob);
 
   const paths = files.map((file) => {
@@ -53,6 +52,9 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { slug } }) {
   const fs = require('fs');
   const files = glob.sync(contentGlob);
+  const renderToString = require('next-mdx-remote/render-to-string');
+
+  const matter = require('gray-matter');
 
   const pathRegex = new RegExp(`^${contentPath}/${path.join(...slug)}.mdx$`);
   const fullPath = files.find((file) => pathRegex.test(file));
