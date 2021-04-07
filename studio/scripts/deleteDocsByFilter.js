ee0409db2f5628565364eaf819af99035fad8e20
Jesse Tomchak <jtomchak@gmail.com>
2021-04-07 10:51:38 -0700

    feat: jam removal and deploy studio gql endpoint

---
+++ studio/scripts/deleteDocsByFilter.js
@@ -0,0 +1,21 @@
+/**
+ * THIS SCRIPT DELETES DATA!
+ *
+ * To use this script:
+ * 1. Put this script in your studio-folder
+ * 2. Write a GROQ filter that outputs the documents you want to delete
+ * 3. Run `sanity dataset export` to backup your dataset before deleting a bunch of documents
+ * 4. Run `sanity exec deleteDocsByFilter.js --with-user-token` to delete the documents
+ *
+ * NOTE: For the time being you should not delete more than ~1000 documents in one transaction. This will change in the future.
+ * See docs:https://www.sanity.io/docs/http-api/http-mutations#deleting-multiple-documents-by-query
+ *
+ * remove all posts by Jesse 'Zitty' T from stage '*[_type == "post" && author._ref == "e-5f8a2ac9edc64a00681eb8ab-self"][0...999]'
+ */
+
+import client from 'part:@sanity/base/client';
+
+client
+  .delete({ query: '*[_type == "aDocumentType"][0...999]' })
+  .then(console.log)
+  .catch(console.error);
