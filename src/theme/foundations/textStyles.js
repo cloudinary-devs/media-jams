7242f182c9718162cbe00a5fa37a90b8bb627f86
Jesse Tomchak <jtomchak@gmail.com>
2021-06-10 15:46:14 -0700

    remove extra typeStyles

--- src/theme/foundations/textStyles.js
+++ src/theme/foundations/textStyles.js
@@ -1,39 +1,4 @@
 export default {
-  H500: {
-    fontSize: { base: '32px', md: '52px' },
-    fontWeight: 'bold',
-    lineHeight: '120%',
-    letterSpacing: '0.02em',
-    // fontFeatureSetting: { base: 'liga 0' },
-  },
-  H400: {
-    fontSize: { base: '28px', md: '38px' },
-    fontWeight: 'bold',
-    lineHeight: { base: '122%', md: '126%' },
-    letterSpacing: '0.01em',
-    // fontFeatureSetting: { base: 'liga 0' },
-  },
-  H300: {
-    fontSize: { base: '26px', md: '28px' },
-    fontWeight: 'bold',
-    lineHeight: { base: '122%', md: '128%' },
-    letterSpacing: '0.01em',
-    // fontFeatureSetting: { base: 'liga 0' },
-  },
-  H200: {
-    fontSize: { base: '22px' },
-    fontWeight: 'bold',
-    lineHeight: { base: '128%' },
-    letterSpacing: '0.01em',
-    // fontFeatureSetting: { base: 'liga 0' },
-  },
-  H100: {
-    fontSize: { base: '17px', md: '18px' },
-    fontWeight: 'bold',
-    lineHeight: { base: '140%', md: '136%' },
-    letterSpacing: '0.01em',
-    // fontFeatureSetting: { base: 'liga 0' },
-  },
   'headline-page': {
     // you can also use responsive styles
     fontFamily: 'Bangers, cursive',
