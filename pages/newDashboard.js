58a7a115805ce4984fdb74ac0fb3b6f2c8e6e816
DomitriusAnthony <domitriusaclark@gmail.com>
2021-06-14 00:24:49 -0400

    Added most of the Hero section and functionality. Need to work on responsive styles.

--- pages/newDashboard.js
+++ pages/newDashboard.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import { Flex, Input, Text, useToken } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import {
+  SimpleGrid,
+  IconButton,
+  Link,
+  Button,
+  Flex,
+  Heading,
+  Input,
+  Text,
+  useToken,
+} from '@chakra-ui/react';
+import {
+  Author,
+  Close,
+  GreenCheck,
+  Stack,
+  Code,
+  Mashups,
+  Video,
+  Pencil,
+} from '@components/Icons';
 
 import { QueryClient, useQuery } from 'react-query';
+import { dehydrate } from 'react-query/hydration';
 import { tags as queryTags } from '@lib/queries/tags';
+import { useUser } from '@auth0/nextjs-auth0';
 
+// Navigation
 function SideStrip() {
   return (
     <Flex
@@ -22,6 +46,127 @@ function SideNav() {
 
 function Tag() {}
 
+// Banner
+function Feature({ children, ...rest }) {
+  return (
+    <Flex w="auto" align="center" {...rest}>
+      <GreenCheck />
+      <Text variant="B400" color="grey.700" pl={3}>
+        {children}
+      </Text>
+    </Flex>
+  );
+}
+
+function FeatureBlock({ header, text, LeftIcon }) {
+  return (
+    <Flex w="280px" direction="column">
+      <Flex align="center">
+        <LeftIcon />
+        <Text ml="12px" variant="B400" fontWeight="bold" color="grey.900">
+          {header}
+        </Text>
+      </Flex>
+      <Text pl={8} mt="5px" variant="B300" color="grey.700">
+        {text}
+      </Text>
+    </Flex>
+  );
+}
+
+function SignupButton() {
+  return (
+    <Link as={NextLink} href="/api/auth/signup">
+      <Button p="6px 32px" borderRadius="8px" bg="primary.500">
+        <Text variant="B400" color="#FFFFFF" fontWeight="700">
+          Sign Up
+        </Text>
+      </Button>
+    </Link>
+  );
+}
+
+function Banner() {
+  const [show, setShow] = React.useState(true);
+  const { user, loading } = useUser();
+
+  React.useEffect(() => {
+    if (user) {
+      setShow(false);
+    }
+  }, [user]);
+
+  return show ? (
+    <Flex
+      bg="#FFFFFF"
+      direction="column"
+      h="420px"
+      w="100%"
+      p="24px"
+      align="center"
+    >
+      <IconButton
+        _hover={{ bg: 'none', outline: 'none' }}
+        alignSelf="flex-end"
+        bg="none"
+        outline="none"
+        w="auto"
+        onClick={() => setShow(false)}
+        icon={<Close />}
+      />
+      <Flex h="100%" w="90%">
+        <Flex w="50%" h="260px" justify="space-evenly" direction="column">
+          <Heading as="h2">Learn Media for Apps</Heading>
+          <Feature mt="17px">Create notes right in the app</Feature>
+          <Feature>Bookmark your favorite jams</Feature>
+          <Feature>Return to your recent jams</Feature>
+          <Flex mt="25px">
+            <SignupButton />
+            <Button p="6px 32px" borderRadius="8px" bg="primary.200" ml={4}>
+              <Text fontWeight="700" variant="B400" color="primary.500">
+                Got It
+              </Text>
+            </Button>
+          </Flex>
+        </Flex>
+        <SimpleGrid columns={2} spacingY={8} spacingX={20} w="520px">
+          <FeatureBlock
+            LeftIcon={Stack}
+            header="Bite-size Tutorials"
+            text="How-to learning guides, limited in scope and easy to follow"
+          />
+          <FeatureBlock
+            LeftIcon={Code}
+            header="Run & Play Sandboxes"
+            text="Learn-by-example demox, open code reeady to run & modify"
+          />
+          <FeatureBlock
+            LeftIcon={Author}
+            header="Authored by Experts"
+            text="Content represents the most advanced dev best practices"
+          />
+          <FeatureBlock
+            LeftIcon={Mashups}
+            header="Tech Stack Mashups"
+            text="Shows media design patterns for most popular tech stacks"
+          />
+          <FeatureBlock
+            LeftIcon={Video}
+            header="Program with Media"
+            text="Create, manage, transform, optimize & deliver images/videos"
+          />
+          <FeatureBlock
+            LeftIcon={Pencil}
+            header="Practical Use-Cases"
+            text="Solutions to media challenges encountered in building apps"
+          />
+        </SimpleGrid>
+      </Flex>
+    </Flex>
+  ) : null;
+}
+
+// Search and Filter
 function Tags() {
   const [selectedFilters, setSelectedFilters] = React.useState([]);
   const { data: jamTagData } = useQuery('jamTags', queryTags.get);
@@ -46,11 +191,47 @@ function Tags() {
     : null;
 }
 
+function TagFilters() {
+  return (
+    <Flex mt="22px" width="95%">
+      <Text variant="B300" color={useToken('colors', 'grey.900')}>
+        Topics:
+      </Text>
+      <Flex w="632px" justify="space-evenly">
+        <Tags />
+      </Flex>
+    </Flex>
+  );
+}
+
+function SearchInput() {
+  return (
+    <Input
+      bg="#FFFFFF"
+      w="100%"
+      h="56px"
+      border="2px solid #88B1FC"
+      borderRadius="8px"
+      boxSizing="border-box"
+      p="0px 16px"
+      mt="24px"
+      placeholder="Search by tag, title, keyword, author, etc..."
+      _placeholder={{
+        fontFamily: 'DM Sans',
+        fontSize: '16px',
+        lineHeight: '152%',
+        color: useToken('colors', 'grey.700'),
+      }}
+    />
+  );
+}
+
+// Page
 export default function NewDashboard() {
   return (
     <Flex bg="#F8F7FC">
       <Flex
-        w="480px"
+        w="auto"
         background="linear-gradient(180deg, #8472DF 0%, #7BCCFF 100%)"
       >
         <SideStrip />
@@ -62,31 +243,12 @@ export default function NewDashboard() {
         overflow="auto"
         direction="column"
         alignItems="center"
+        justify="center"
       >
-        <Input
-          bg="#FFFFFF"
-          w="888px"
-          h="56px"
-          border="2px solid #88B1FC"
-          borderRadius="8px"
-          boxSizing="border-box"
-          p="0px 16px"
-          mt="24px"
-          placeholder="Search by tag, title, keyword, author, etc..."
-          _placeholder={{
-            fontFamily: 'DM Sans',
-            fontSize: '16px',
-            lineHeight: '152%',
-            color: useToken('colors', 'grey.700'),
-          }}
-        />
-        <Flex mt="22px" width="888px">
-          <Text variant="B300" color={useToken('colors', 'grey.900')}>
-            Topics:{' '}
-          </Text>
-          <Flex w="632px" justify="space-evenly">
-            <Tags />
-          </Flex>
+        <Banner />
+        <Flex direction="column" w="90%">
+          <SearchInput />
+          <TagFilters />
         </Flex>
       </Flex>
     </Flex>
---
+++ src/components/Icons/index.js
@@ -0,0 +1,10 @@
+import Author from './Author';
+import Close from './Close';
+import GreenCheck from './GreenCheck';
+import Stack from './Stack';
+import Code from './Code';
+import Mashups from './Mashups';
+import Video from './Video';
+import Pencil from './Pencil';
+
+export { Author, Close, GreenCheck, Stack, Code, Mashups, Video, Pencil };
