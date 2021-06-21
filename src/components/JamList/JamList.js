16103fed27410fdecd057069a6c29b195eee08f8
DomitriusAnthony <domitriusaclark@gmail.com>
2021-06-21 03:20:52 -0400

    Early Featured card styles in. Still messing with sizing and adding featured callout

--- src/components/JamList/JamList.js
+++ src/components/JamList/JamList.js
@@ -1,17 +1,26 @@
+import React from 'react';
 import { Flex } from '@chakra-ui/react';
 import JamCard from './JamCard';
+import FeaturedJamCard from './FeaturedJamCard';
 
 export default function JamList({ jams }) {
+  const [featuredJam, setFeaturedJam] = React.useState();
+
+  React.useEffect(() => {
+    const takeFirstJam = jams.shift();
+    setFeaturedJam(takeFirstJam);
+  }, [jams]);
   return (
     <Flex
-      mt="26px"
       w="1000px"
+      mt="26px"
       alignSelf="center"
       h="100%"
       direction="column"
       justify="space-around"
       sx={{ gap: '16px' }}
     >
+      {featuredJam && <FeaturedJamCard jam={featuredJam} />}
       {jams.map((jam) => (
         <JamCard jam={jam} />
       ))}
