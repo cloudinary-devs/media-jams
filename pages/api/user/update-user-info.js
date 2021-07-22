ffd374d176b55a8b433f3ebaa4c027c1b125b804
DomitriusAnthony <domitriusaclark@gmail.com>
2021-07-22 04:56:03 -0400

    Got account details changing successfully and added toasts for success and failure.

--- pages/api/user/update-user-info.js
+++ pages/api/user/update-user-info.js
@@ -1,25 +1,11 @@
-import { updateUserSettings, checkAuthCredentials } from '@lib/auth0Management';
+import { updateUserSettings } from '@lib/auth0Management';
 
 export default async function handler(req, res) {
   const { body } = req;
 
-  const { email, password, userId, name, picture } = JSON.parse(body);
+  const { email, userId, name, picture } = JSON.parse(body);
 
-  const authResponse = await checkAuthCredentials(email, password);
-  const authResult = await authResponse.json();
+  const updatedUser = await updateUserSettings(name, email, picture, userId);
 
-  const token = authResult.access_token;
-
-  if (authResult.hasOwnProperty('error')) {
-    res.status(401).send(authResult);
-  } else {
-    const updatedUser = await updateUserSettings(
-      name,
-      email,
-      picture,
-      userId,
-      token,
-    );
-    res.send(updatedUser);
-  }
+  res.send(updatedUser.status);
 }
--- src/components/AccountSettings/AccountSettingsForm.js
+++ src/components/AccountSettings/AccountSettingsForm.js
@@ -1,181 +1,139 @@
 import {
   Flex,
   Heading,
+  Link,
   Text,
   Stack,
   Button,
   StackDivider,
   Input,
   HStack,
-  VStack,
   FormControl,
   FormLabel,
   Box,
   Avatar,
   useColorModeValue,
+  createStandaloneToast,
 } from '@chakra-ui/react';
-
 import { useForm } from 'react-hook-form';
-
 import { HiCloudUpload } from 'react-icons/hi';
-import FieldGroup from '@components/FieldGroup';
 
-function FormHeading({ register }) {
+function FormHeading() {
   return (
     <Flex w="100%" justify="space-between">
       <Heading size="H300" paddingBottom="4">
         Account Settings
       </Heading>
-      <Flex justify="space-evenly" align="center">
-        <Input
-          height="48px"
-          width="456px"
-          mr="8px"
-          placeholder="Current Password (to save changes)"
-          type="password"
-          {...register('password')}
-        />
-        <Button
-          type="submit"
-          bg="primary.500"
-          color="white"
-          w="100px"
-          height="40px"
-        >
-          <Text variant="B300" fontWeight="bold">
-            Save
-          </Text>
-        </Button>
-      </Flex>
+      <Button
+        type="submit"
+        bg="primary.500"
+        color="white"
+        w="100px"
+        height="40px"
+      >
+        <Text variant="B300" fontWeight="bold">
+          Save
+        </Text>
+      </Button>
     </Flex>
   );
 }
 
-function PersonalInfoFieldGroup({ register }) {
+function NameFieldGroup({ register, name }) {
   return (
-    <FieldGroup>
-      <Flex direction="column" w="100%">
-        <Heading size="H200" mb="52px">
-          Personal information
-        </Heading>
-        <VStack width="100%" spacing="6">
-          <Flex direction="column" w="100%">
-            <FormControl id="name">
-              <Flex w="65%" justify="space-between">
-                <FormLabel>
-                  <Text color="grey.900" variant="B200" fontWeight="bold">
-                    Name
-                  </Text>
-                </FormLabel>
-                <Input
-                  height="48px"
-                  width="456px"
-                  type="text"
-                  {...register('name')}
-                />
-              </Flex>
-            </FormControl>
-          </Flex>
-
-          <Flex direction="column" w="100%">
-            <FormControl id="email">
-              <Flex width="65%" justify="space-between">
-                <FormLabel>
-                  <Text color="grey.900" variant="B200" fontWeight="bold">
-                    Email
-                  </Text>
-                </FormLabel>
-                <Input
-                  height="48px"
-                  width="456px"
-                  type="email"
-                  {...register('email', {
-                    pattern: {
-                      value: /\S+@\S+\.\S+/,
-                      message: 'Entered value does not match email format',
-                    },
-                  })}
-                />
-              </Flex>
-            </FormControl>
-          </Flex>
-        </VStack>
-      </Flex>
-    </FieldGroup>
+    <Flex direction="column">
+      <Heading size="H200" mb="52px">
+        Personal information
+      </Heading>
+      <FormControl justifyContent="flex-start" id="name">
+        <FormLabel display="flex" w="50px">
+          <Text mb="20px" color="grey.900" variant="B200" fontWeight="bold">
+            Name
+          </Text>
+        </FormLabel>
+        <Input
+          height="48px"
+          width="456px"
+          type="text"
+          defaultValue={name}
+          {...register('name')}
+        />
+      </FormControl>
+    </Flex>
   );
 }
 
-function NewPasswordFieldGroup({ register }) {
+function EmailFieldGroup({ email, register }) {
   return (
-    <FieldGroup>
-      <Flex direction="column" w="100%">
-        <VStack width="100%" spacing="6">
-          <Flex direction="column" w="100%">
-            <FormControl id="name">
-              <Flex width="65%" justify="space-between">
-                <FormLabel>
-                  <Text color="grey.900" variant="B200" fontWeight="bold">
-                    Password
-                  </Text>
-                </FormLabel>
-                <Flex direction="column">
-                  <Input
-                    height="48px"
-                    width="456px"
-                    type="password"
-                    placeholder="Change your password"
-                    {...register('newPassword')}
-                  />
-                </Flex>
-              </Flex>
-            </FormControl>
-          </Flex>
-        </VStack>
-      </Flex>
-    </FieldGroup>
+    <FormControl justifyContent="flex-start" id="name">
+      <FormLabel display="flex" w="50px">
+        <Text mb="20px" color="grey.900" variant="B200" fontWeight="bold">
+          Email
+        </Text>
+      </FormLabel>
+      <Input
+        height="48px"
+        width="456px"
+        type="email"
+        defaultValue={email}
+        {...register('email')}
+      />
+    </FormControl>
+  );
+}
+
+function PasswordFieldGroup() {
+  return (
+    <Flex direction="column" w="100%">
+      <Text mb="20px" color="grey.900" variant="B200" fontWeight="bold">
+        Password
+      </Text>
+      <Link>
+        <Text color="grey.900" variant="B200" fontWeight="bold">
+          Change password
+        </Text>
+      </Link>
+    </Flex>
   );
 }
 
 function PictureFieldGroup({ user }) {
   return (
-    <FieldGroup>
-      <Flex direction="column" w="100%">
-        <Heading size="H200" mb="52px">
-          Photo
-        </Heading>
-        <Stack
-          direction="row"
-          spacing="6"
-          align="center"
-          width="50%"
-          justify="space-between"
-        >
-          <Avatar size="3xl" name={user.name} src={user.picture} />
-          <Box>
-            <HStack spacing="5">
-              <Button leftIcon={<HiCloudUpload />}>Change photo</Button>
-              <Button variant="ghost" colorScheme="red">
-                Delete
-              </Button>
-            </HStack>
-            <Text
-              fontSize="sm"
-              mt="3"
-              color={useColorModeValue('gray.500', 'whiteAlpha.600')}
-            >
-              .jpg, .gif, or .png. Max file size 700K.
-            </Text>
-          </Box>
-        </Stack>
-      </Flex>
-    </FieldGroup>
+    <Flex direction="column" w="100%">
+      <Heading size="H200" mb="52px">
+        Photo
+      </Heading>
+      <Stack
+        direction="row"
+        spacing="6"
+        align="center"
+        width="50%"
+        justify="space-between"
+      >
+        <Avatar size="3xl" name={user.name} src={user.picture} />
+        <Box>
+          <HStack spacing="5">
+            <Button leftIcon={<HiCloudUpload />}>Change photo</Button>
+            <Button variant="ghost" colorScheme="red">
+              Delete
+            </Button>
+          </HStack>
+          <Text
+            fontSize="sm"
+            mt="3"
+            color={useColorModeValue('gray.500', 'whiteAlpha.600')}
+          >
+            .jpg, .gif, or .png. Max file size 700K.
+          </Text>
+        </Box>
+      </Stack>
+    </Flex>
   );
 }
 
 export default function AccountSettingsForm({ user }) {
-  console.log();
+  const toast = createStandaloneToast();
 
-  const currentEmail = user.email;
   const {
     register,
     handleSubmit,
@@ -183,29 +141,43 @@ export default function AccountSettingsForm({ user }) {
   } = useForm({
     defaultValues: {
       name: user.name,
-      email: user.email,
       picture: user.picture,
     },
   });
 
   async function submitFormValues(data) {
-    const userId = user.sub.substring(user.sub.indexOf('|') + 1);
-    console.log(userId);
-    console.log(user.sub);
-
-    await fetch('/api/user/update-user-info', {
+    const res = await fetch('/api/user/update-user-info', {
       method: 'POST',
-      body: JSON.stringify({ userId: user.sub, currentEmail, ...data }),
+      body: JSON.stringify({ ...data, email: user.email, userId: user.sub }),
     });
+
+    if (res.status === 200) {
+      toast({
+        title: 'Successfully updated your profile',
+        status: 'success',
+        duration: 3000,
+        position: 'top',
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: 'Something went wrong! Try again.',
+        status: 'error',
+        duration: 3000,
+        position: 'top',
+        isClosable: true,
+      });
+    }
   }
 
   return (
     <Flex w="100%" justify="center">
-      <form onSubmit={handleSubmit(submitFormValues)} style={{ width: '70%' }}>
+      <form onSubmit={handleSubmit(submitFormValues)} style={{ width: '100%' }}>
         <Stack ml={8} spacing="4" divider={<StackDivider />}>
           <FormHeading register={register} />
-          <PersonalInfoFieldGroup register={register} />
-          <NewPasswordFieldGroup register={register} />
+          <NameFieldGroup name={user.name} register={register} />
+          <EmailFieldGroup register={register} email={user.email} />
+          <PasswordFieldGroup />
           <PictureFieldGroup user={user} />
         </Stack>
       </form>
