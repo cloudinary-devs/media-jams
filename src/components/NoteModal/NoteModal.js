899beeb8e16ea55ab17aacb828aa054628a8075f
Jesse Tomchak <jtomchak@gmail.com>
2021-07-22 16:57:47 -0700

    fix: able to make new notes

--- src/components/NoteModal/NoteModal.js
+++ src/components/NoteModal/NoteModal.js
@@ -18,55 +18,61 @@ import { notes } from '@lib/queries/notes';
 
 export default function NoteModal({ isOpen, onClose }) {
   const [title, setTitle] = React.useState('');
-  const [note, setNote] = React.useState('');
+  const [currentNote, setNote] = React.useState('');
   const queryClient = useQueryClient();
   const toast = createStandaloneToast();
 
-  const createNote = useMutation((note) => notes.add({ title, body: note }), {
-    onMutate: async (note) => {
-      setNote('');
-      await queryClient.cancelQueries('notes');
-      const previousValue = queryClient.getQueryData('notes');
+  const createNote = useMutation(
+    ({ title, note }) => notes.add({ title, body: JSON.stringify(note) }),
+    {
+      onMutate: async ({ title, note }) => {
+        setTitle('');
+        setNote('');
+        await queryClient.cancelQueries('notes');
+        const previousValue = queryClient.getQueryData('notes');
 
-      queryClient.setQueryData('notes', (old) => {
-        if (old) {
-          return {
-            ...old,
-            notes: [...old?.notes, { title, body: note }],
-          };
-        } else {
-          return {
-            notes: [{ title, body: note }],
-          };
-        }
-      });
+        queryClient.setQueryData('notes', (old) => {
+          if (old) {
+            return {
+              ...old,
+              notes: [...old?.notes, { title, body: note }],
+            };
+          } else {
+            return {
+              notes: [{ title, body: note }],
+            };
+          }
+        });
 
-      return previousValue;
+        return previousValue;
+      },
+      // On failure, roll back to the previous value
+      onError: (err, variables, previousValue) => {
+        queryClient.setQueryData('notes', previousValue);
+        toast({
+          title: 'Oh no!',
+          description: 'Something went wrong while creating your note!',
+          status: 'error',
+          duration: 3000,
+          position: 'top',
+          isClosable: true,
+        });
+      },
+      // After success, refetch the notes query
+      onSuccess: () => {
+        queryClient.invalidateQueries('notes');
+        onClose(
+          toast({
+            title: 'Successfully created your note!',
+            status: 'success',
+            duration: 3000,
+            position: 'top',
+            isClosable: true,
+          }),
+        );
+      },
     },
-    // On failure, roll back to the previous value
-    onError: (err, variables, previousValue) => {
-      queryClient.setQueryData('notes', previousValue);
-      toast({
-        title: 'Oh no!',
-        description: 'Something went wrong while creating your note!',
-        status: 'error',
-        duration: 3000,
-        position: 'top',
-        isClosable: true,
-      });
-    },
-    // After success, refetch the notes query
-    onSuccess: () => {
-      queryClient.invalidateQueries('notes');
-      toast({
-        title: 'Successfully created your note!',
-        status: 'success',
-        duration: 3000,
-        position: 'top',
-        isClosable: true,
-      });
-    },
-  });
+  );
   return (
     <Modal size="3xl" onClose={onClose} isOpen={isOpen} isCentered>
       <ModalOverlay />
@@ -79,9 +85,9 @@ export default function NoteModal({ isOpen, onClose }) {
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
-          <NoteEditor note={note} setNote={setNote} />
+          <NoteEditor note={currentNote} setNote={setNote} />
           <Button
-            onClick={() => createNote.mutate({ title, ...note })}
+            onClick={() => createNote.mutate({ title, note: currentNote })}
             mt="6px"
             mb="12px"
             alignSelf="flex-end"
--- src/components/Sidebar/SideContent/NotesPanel.js
+++ src/components/Sidebar/SideContent/NotesPanel.js
@@ -1,6 +1,40 @@
-import { Box, Stack, Heading, Image, Button } from '@chakra-ui/react';
+import React from 'react';
+import {
+  Box,
+  Text,
+  Heading,
+  Stack,
+  Flex,
+  Avatar,
+  Link,
+  useColorModeValue,
+  createStandaloneToast,
+  Spacer,
+  IconButton,
+  Button,
+  Image,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
+import format from 'date-fns/format';
+import { useNotesQuery } from '@hooks/useNotes';
 
-const Notes = () => {
+import { SearchFieldInput } from './SearchFieldInput';
+import { Trashcan } from '@components/Icons';
+
+import Fuse from 'fuse.js';
+
+const fuseOptions = {
+  threshold: 0.35,
+  location: 0,
+  distance: 100,
+  minMatchCharLength: 1,
+  includeScore: true,
+  useExtendedSearch: true,
+  isCaseSensitive: true,
+  keys: ['title', 'tags.title', 'author.name'],
+};
+
+const EmptyNotes = ({ user }) => {
   return (
     <Stack
       spacing={16}
@@ -10,16 +44,148 @@ const Notes = () => {
       textAlign="center"
       alignItems="center"
     >
-      <Box size="xs">
-        <Image src="/emptyNotes.svg" alt="notes section empty" />
-      </Box>
-      <Heading size="H200" color="primary.900">
-        You need to sign up to add notes
-      </Heading>
-      <Button as="a" size="md" colorScheme="primary" href="/api/auth/signup">
-        Sign Up
-      </Button>
+      {user ? (
+        <>
+          <Box size="xs">
+            <Image src="/emptyBookmarks.svg" alt="bookmarks empty" />
+          </Box>
+          <Heading size="H200" color="primary.900">
+            You don't have any notes
+          </Heading>
+        </>
+      ) : (
+        <>
+          <Box size="xs">
+            <Image src="/emptyNotes.svg" alt="notes section empty" />
+          </Box>
+          <Heading size="H200" color="primary.900">
+            You need to sign up to add notes
+          </Heading>
+          <Button
+            as="a"
+            size="md"
+            colorScheme="primary"
+            href="/api/auth/signup"
+          >
+            Sign Up
+          </Button>
+        </>
+      )}
     </Stack>
+  );
+};
+
+export const NoteCard = ({ note, ...props }) => {
+  const toast = createStandaloneToast();
+  // const removeBookmark = useRemoveBookmarkMutation({
+  //   onSuccess: () => {
+  //     // toast message
+  //     toast({
+  //       title: `Bookmark: ${jam.title} removed.`,
+  //       status: 'success',
+  //       duration: 2000,
+  //       isClosable: true,
+  //       position: 'top',
+  //     });
+  //   },
+  // });
+  const { title, body } = note;
+  return (
+    <Box
+      bg={useColorModeValue('white', 'gray.700')}
+      border="1px solid #D3DDE6"
+      borderRadius="8px"
+      maxWidth="2xl"
+      marginTop={2}
+      p={{ base: '2', md: '3' }}
+      shadow={{ md: 'base' }}
+      {...props}
+    >
+      <Stack
+        direction="row"
+        spacing="4"
+        spacing={{ base: '1', md: '2' }}
+        alignItems="center"
+      >
+        <NextLink href={`/`} passHref>
+          <Link>
+            <Text fontSize="md" variant="B500" color="gray.800">
+              {title}
+            </Text>
+          </Link>
+        </NextLink>
+        <Spacer />
+        <IconButton
+          onClick={() => {}}
+          variant="ghost"
+          aria-label="Remove Note"
+          icon={<Trashcan />}
+        />
+      </Stack>
+      <Flex paddingTop={{ base: 0, md: '2' }} alignItems="start">
+        <Heading size="H100">{title}</Heading>
+      </Flex>
+    </Box>
+  );
+};
+
+const Notes = ({ user = null }) => {
+  const [filteredNotes, setFilteredNotes] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState('');
+  const { isLoading, data } = useNotesQuery();
+  React.useEffect(() => {
+    if (!searchValue) {
+      handleFilter(data?.notes);
+    } else {
+      const queries = {
+        $or: [
+          { title: searchValue },
+          {
+            $path: ['author.name'],
+            $val: searchValue,
+          },
+          {
+            $path: 'tags.title',
+            $val: searchValue,
+          },
+        ],
+      };
+      const results = fuse.search(queries).map((result) => result.item);
+      handleFilter(results);
+    }
+  }, [searchValue, data]);
+  // Set Fuse
+  const fuse = new Fuse(data?.notes, fuseOptions);
+  const onChange = (e) => {
+    const { value } = e.target;
+    setSearchValue(value);
+  };
+  const handleFilter = (data) => {
+    setFilteredNotes(data);
+  };
+
+  return (
+    <Flex
+      width={{ base: 'full' }}
+      direction="column"
+      px={{ base: 3, md: 6 }}
+      pb={8}
+      overflowY="auto"
+    >
+      {/* check user
+        no notes and nothing in the search show empty
+     */}
+      {!user || (filteredNotes?.length === 0 && !searchValue) ? (
+        <EmptyNotes user={user} />
+      ) : (
+        <Stack>
+          <SearchFieldInput value={searchValue} onChange={onChange} mb={6} />
+          {filteredNotes?.map((note) => (
+            <NoteCard key={note._id} note={note} />
+          ))}
+        </Stack>
+      )}
+    </Flex>
   );
 };
 
