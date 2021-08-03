11b68bac1286dfb1ac836654a11f55ddfd6aa60d
Jesse Tomchak <jtomchak@gmail.com>
2021-08-03 11:15:14 -0700

    chore: add number of authors above cards (#172)

--- src/components/Sidebar/SideContent/LoadingSkeleton.js
+++ src/components/Sidebar/SideContent/LoadingSkeleton.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Box, SkeletonCircle, SkeletonText } from '@chakra-ui/react';
 
-export const LoadingSkeleton = ({ isLoading = false }) =>
+export const LoadingSkeleton = ({ isLoading = false, children }) =>
   isLoading ? (
     <Box borderRadius="8px" maxWidth="2xl">
       <SkeletonCircle isLoaded={!isLoading} size="10" />
       <SkeletonText isLoaded={!isLoading} mt="4" noOfLines={4} spacing="4" />
     </Box>
-  ) : null;
+  ) : (
+    <>{children}</>
+  );
