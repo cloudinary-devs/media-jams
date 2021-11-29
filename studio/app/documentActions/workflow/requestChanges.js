45b8c47ee7c50984b876c041c28838165ab7d23f
Jesse Tomchak <jtomchak@gmail.com>
2021-11-29 16:42:51 -0700

    once more cleaning logs

--- studio/app/documentActions/workflow/requestChanges.js
+++ studio/app/documentActions/workflow/requestChanges.js
@@ -3,7 +3,6 @@ import { userModerator } from '../../lib/user';
 import { inferMetadataState, useWorkflowMetadata } from '../../lib/workflow';
 
 export function requestChangesAction(props) {
-  console.log('requestChange', props);
   const metadata = useWorkflowMetadata(props.id, inferMetadataState(props));
   const buttonDisabled = !userModerator();
   // if it's not in the following state, do not show
