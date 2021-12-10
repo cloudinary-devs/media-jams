de6e9c639f4eff1982c3a2d0c2441ab71abcf527
Jesse Tomchak <jtomchak@gmail.com>
2021-12-10 14:42:40 -0700

    fix sidebar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,8 @@
 import { Flex } from '@chakra-ui/react';
 import { useUser } from '@auth0/nextjs-auth0';
-import dynamic from 'next/dynamic';
+import Sidebar from '@components/Sidebar';
 import { MobileTopBar } from '@components/Sidebar/MobileTopBar';
 import { useSidePanel } from '@components/SidePanelProvider';
-
-const Sidebar = dynamic(() => import('@components/Sidebar'), {});
 
 export default function Layout({ children }) {
   const { user } = useUser();
