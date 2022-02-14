5dfac1d935ae18b4bd0b3d7f3b201ad5d6f42eed
Jesse Tomchak <jtomchak@gmail.com>
2022-02-14 11:10:44 -0700

    Author Fixes (#309)
    * fixed post default to post-by-author
    
    * markdown update

--- studio/app/structure/newDocument.js
+++ studio/app/structure/newDocument.js
@@ -1,6 +1,4 @@
 import S from '@sanity/base/structure-builder';
 import { GoFile } from 'react-icons/go';
 
-export default [
-  S.initialValueTemplateItem('post').id(`post`).title('New Media Jam'),
-];
+export default [S.initialValueTemplateItem('post-by-author')];
