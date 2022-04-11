f930a61d4734f394fd12fdf4021e8442a9ee1f96
colbyfayock <fayock@gmail.com>
2022-04-11 16:40:20 -0300

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -39,98 +39,13 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Bangers&family=Roboto&display=swap"
             rel="stylesheet"
           />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="57x57"
-            href="/favicons/apple-touch-icon-57x57.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="114x114"
-            href="/favicons/apple-touch-icon-114x114.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="72x72"
-            href="/favicons/apple-touch-icon-72x72.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="144x144"
-            href="/favicons/apple-touch-icon-144x144.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="60x60"
-            href="/favicons/apple-touch-icon-60x60.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="120x120"
-            href="/favicons/apple-touch-icon-120x120.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="76x76"
-            href="/favicons/apple-touch-icon-76x76.png"
-          />
-          <link
-            rel="apple-touch-icon-precomposed"
-            sizes="152x152"
-            href="/favicons/apple-touch-icon-152x152.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="/favicons/favicon-196x196.png"
-            sizes="196x196"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="/favicons/favicon-96x96.png"
-            sizes="96x96"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="/favicons/favicon-32x32.png"
-            sizes="32x32"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="/favicons/favicon-16x16.png"
-            sizes="16x16"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            href="/favicons/favicon-128.png"
-            sizes="128x128"
-          />
-          <meta name="application-name" content="&nbsp;" />
-          <meta name="msapplication-TileColor" content="#FFFFFF" />
-          <meta
-            name="msapplication-TileImage"
-            content="favicons/mstile-144x144.png"
-          />
-          <meta
-            name="msapplication-square70x70logo"
-            content="favicons/mstile-70x70.png"
-          />
-          <meta
-            name="msapplication-square150x150logo"
-            content="favicons/mstile-150x150.png"
-          />
-          <meta
-            name="msapplication-wide310x150logo"
-            content="favicons/mstile-310x150.png"
-          />
-          <meta
-            name="msapplication-square310x310logo"
-            content="favicons/mstile-310x310.png"
-          />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1b1464" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <ColorModeScript defaultMode="system" />
